8a2ef6e75c6545c6f99f003618600f8a99a42b97
RaghavKalani <raghavkalani2225@gmail.com>
2026-02-23 10:21:39 +0530

    Resolved merge conflict with develop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,7 +2,6 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         String input = "madam";
-<<<<<<< HEAD
         boolean isPalindrome = true;
 
         for (int i=0; i<input.length()/2; i++) {
@@ -13,7 +12,6 @@ public class PalindromeCheckerApp {
         }
 
         if (isPalindrome) {
-=======
         String reversed = "";
 
         for (int i = input.length() - 1; i >= 0; i--) {
@@ -21,7 +19,6 @@ public class PalindromeCheckerApp {
         }
 
         if (input.equals(reversed)) {
->>>>>>> feature/UC3
             System.out.println(input + " is a Palindrome.");
         } else {
             System.out.println(input + " is not a Palindrome.");
