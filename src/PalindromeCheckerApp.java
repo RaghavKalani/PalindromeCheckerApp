4d5b32f6344ec81911a04dd1ed6f797634c8506a
RaghavKalani <raghavkalani2225@gmail.com>
2026-02-23 10:28:09 +0530

    Resolved merge conflict and finalized UC3 logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,11 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
         String input = "madam";
-        boolean isPalindrome = true;
-
-        for (int i=0; i<input.length()/2; i++) {
-            if (input.charAt(i) != input.charAt(input.length()-1-i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
         String reversed = "";
 
-        for (int i = input.length() - 1; i >= 0; i--) {
+        for (int i=input.length()-1; i>=0; i--) {
             reversed = reversed + input.charAt(i);
         }
 
