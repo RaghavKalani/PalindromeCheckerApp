a35d3a840a84629f683ce11b799e624c415abf6a
RaghavKalani <raghavkalani2225@gmail.com>
2026-02-23 10:39:20 +0530

    Final clean UC3 implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,10 @@
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
 
         String input = "madam";
         String reversed = "";
 
-        for (int i=input.length()-1; i>=0; i--) {
+        for (int i = input.length()-1; i>=0; i--) {
             reversed = reversed + input.charAt(i);
         }
 
