bd1be9a0087b5a20985bf2c6674dbd0aa86fad34
RaghavKalani <raghavkalani2225@gmail.com>
2026-02-23 09:53:28 +0530

    Update UC2: Implemented two-pointer palindrome logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,8 +4,8 @@ public class PalindromeCheckerApp {
         String input = "madam";
         boolean isPalindrome = true;
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        for (int i=0; i<input.length()/2; i++) {
+            if (input.charAt(i) != input.charAt(input.length()-1-i)) {
                 isPalindrome = false;
                 break;
             }
